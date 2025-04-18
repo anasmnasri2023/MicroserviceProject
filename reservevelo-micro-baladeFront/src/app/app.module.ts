e8127118af10d4893f5b4e19b0f6ba2b67ff9de8
anasmnasri2023 <anes.mnasri@esprit.tn>
2025-04-18 21:32:17 +0200

    FrontBalade

--- reservevelo-micro-baladeFront/src/app/app.module.ts
+++ reservevelo-micro-baladeFront/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/components/header/header.component';
+import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HomeComponent } from './components/home/home.component';
 import { AddbaladeComponent } from './balade/addbalade/addbalade.component';
