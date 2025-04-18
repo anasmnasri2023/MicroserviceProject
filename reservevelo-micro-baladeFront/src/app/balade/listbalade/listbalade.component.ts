c1918334c6c4e917825da827574ac859ace55e33
anasmnasri2023 <anes.mnasri@esprit.tn>
2025-04-18 22:00:51 +0200

    FrontBalade

--- reservevelo-micro-baladeFront/src/app/balade/listbalade/listbalade.component.ts
+++ reservevelo-micro-baladeFront/src/app/balade/listbalade/listbalade.component.ts
@@ -1,11 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BaladeserviceService, Balade } from '../../services/baladeservice.service';
 
 @Component({
   selector: 'app-listbalade',
-  standalone: false,
   templateUrl: './listbalade.component.html',
-  styleUrl: './listbalade.component.css'
+  styleUrls: ['./listbalade.component.css']
 })
-export class ListbaladeComponent {
+export class ListbaladeComponent implements OnInit {
+  balades: Balade[] = [];
 
+  constructor(private baladeService: BaladeserviceService) {}
+
+  ngOnInit(): void {
+    this.loadBalades();
+  }
+
+  loadBalades(): void {
+    this.baladeService.getAllBalades().subscribe({
+      next: (data) => this.balades = data,
+      error: (err) => console.error('Erreur chargement des balades', err)
+    });
+  }
+
+  deleteBalade(id: string): void {
+    this.baladeService.deleteBalade(id).subscribe(() => {
+      this.loadBalades(); // refresh après suppression
+    });
+  }
 }
+
+
+
+
+
+
+
+
+
+
+
+
